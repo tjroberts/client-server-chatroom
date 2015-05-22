95af996b38bce23424bcca85df8e4f80159dfc2e
tjrobert <tyroberts7846@yahoo.com>
2015-05-21 21:26:24 -0700

    MVC Server
    Couldn't make many changes because we don't actually create an instance
    of the ServerControl (Handler) in this one.
    
    Its still organized better though...

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 from network import Listener, Handler, poll
 
- 
-handlers = {}  # map client handler to user name
+#could delcare all relevant data here, like the scripts for interacting with users
+class ServerModel:
+    
+    def __init__(self):
+        pass
+
+class ServerView:
+    
+    @staticmethod     #decorator for declaring static methods
+    def display(data):
+        print(data)
+
  
 #MyHandler inherits from Handler, we do this so we can pass it to the Listener below
-class MyHandler(Handler):
+#this is similar to controller
+class ServerControl(Handler):
     
     def on_open(self):
         pass
@@ -16,22 +27,25 @@ class MyHandler(Handler):
         
         #server figures out what to do depending on keys in dict
         if ( 'join' in msg ):
-            print("{} has joined the chat!".format(msg['join']))
+            ServerView.display("{} has joined the chat!".format(msg['join']))
         elif ( 'data' in msg ): #server has been sent just data
             
             if ( msg['data'].lower() == "ping" ): #if the contents of data msg are ping
-                print("got ping")
+                ServerView.display("got ping")
                 self.do_send("ping") #just send ping back
             
         elif ('speak' in msg ): #if chat message
-            print("{}: {}".format(msg['speak'], msg['txt'])); # just print what they are saying
+            ServerView.display("{}: {}".format(msg['speak'], msg['txt'])); # just print what they are saying
         else:
             pass
 
+if __name__ == "__main__" :
 
-port = 8888
-server = Listener(port, MyHandler)
-while 1:
-    poll(timeout=0.05) # in seconds
+    handlers = {}  # map client handler to user name
 
+    port = 8888
+    server = Listener(port, ServerControl)
+    while 1:
+        poll(timeout=0.05) # in seconds
 
+    
