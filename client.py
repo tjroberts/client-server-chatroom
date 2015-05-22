a48bc5f949ad5e9740d606a2d9ccdd6725ff1a68
tjrobert <tyroberts7846@yahoo.com>
2015-05-21 20:50:20 -0700

    MVC Client
    Implemented MVC on client side.
    Note:
    Handler child class Client becomes controller in MVC

--- client.py
+++ client.py
@@ -5,77 +5,112 @@ from time import sleep
 
 import time
 
-TIMEOUT_VAL = 2  #seconds
-havePingResponse = False
-endTime = 0
+#could delcare all relevant data here, like the scripts for interacting with users
+class ClientModel:
+    
+    TIMEOUT_VAL = 2 #seconds
+    
+    def __init__(self):
+	pass
+	
+    def get_poll_timeout(self):
+	return self.TIMEOUT_VAL
+    
 
-myname = raw_input('What is your name? ')
-
-class Client(Handler):
+#view handles all communication with console (the view for our application, what people interact with)
+class ClientView:
+	
+    def get_user_input(self, message = ""):
+	
+	if ( message == "" ):
+	    return sys.stdin.readline().rstrip()
+	else:
+	    return raw_input(message)
+	
+    def display(self, data):
+	print(data)
+    
+    
+#controller inherits from Handler (handler controls interactions so its logical to use this as control for MVC)
+class ClientControl(Handler):
+    
+    model = None
+    view = None
+    client = None
+    
+    havePingResponse = False
+    endTime = 0
     
     def on_close(self):
-        pass
+	pass
     
     def on_msg(self, msg): #right now client is assuming server is just sending pure data, no dictionaries
-        global havePingResponse #have to use global keyword in python, since python uses Dynamic Binding
-        global endTime
 
-        #do not print ping response, just record when message back is received and set flag
-        if ( msg.lower() == "ping" ):
-            havePingResponse = True
-            endTime = time.time() * 1000
-        else:
-            print("Server says " + msg)
+	#do not print ping response, just record when message back is received and set flag
+	if ( msg.lower() == "ping" ):
+	    self.havePingResponse = True #use owner to access outer class
+	    self.endTime = time.time() * 1000
+	else: 
+	    view.display("Server says" + data)
+	
+    #no need for self since this will only be called internally
+    def periodic_poll(self):
+	while 1:
+	    poll()
+	    sleep(0.05)  # seconds
 
-        
-host, port = 'localhost', 8888
-client = Client(host, port)
-client.do_send({'join': myname})
+    def start_control(self, model, view):
 
-def periodic_poll():
-    while 1:
-        poll()
-        sleep(0.05)  # seconds
-                            
-thread = Thread(target=periodic_poll)
-thread.daemon = True  # die when the main thread dies 
-thread.start()
+	self.model = model
+	self.view = view
+	
+	TIMEOUT_VAL = model.get_poll_timeout() #get timeout value from model
+	
+	myname = view.get_user_input('What is your name? ')
+	self.do_send({'join': myname})
+	
+	thread = Thread(target=self.periodic_poll)
+	thread.daemon = True  # die when the main thread dies 
+	thread.start()
 
-#always need to send name before any other input
-#client.do_send({'speak': myname, 'txt': mytxt})
+	while 1:
+	    
+	    #get user input, blocking call (strip whitespace for sending messages)
+	    mytxt = view.get_user_input()
 
-while 1:
-    
-	#get user input, blocking call (strip whitespace for sending messages)
-    mytxt = sys.stdin.readline().rstrip()
+	    #check if user is sending ping
+	    if ( mytxt.lower() == "ping" ):
+		
+		view.display("Pinging chat server, sending {} bytes.".format(sys.getsizeof("ping")));
 
-	#check if user is sending ping
-    if ( mytxt.lower() == "ping" ) :
-
-        print("Pinging chat server, sending {} bytes.".format(sys.getsizeof("ping")));
-
-        client.do_send({'data':mytxt}) #data key when sending data to server
+		self.do_send({'data':mytxt}) #data key when sending data to server
             
-        #time ping interaction			
-        startTime = time.time() * 1000   #milliseconds
+		#time ping interaction			
+		startTime = time.time() * 1000   #milliseconds
 
-        #wait for response (use global but don't need keyword in since we aren't re-defining)
-        poll_for(TIMEOUT_VAL) # in seconds
+		#wait for response (use global but don't need keyword in since we aren't re-defining)
+		poll_for(TIMEOUT_VAL) # in seconds
         
 		#check if the flag has been ticked (got ping response back)
-        if ( (not havePingResponse) ):
-            print("No response from server, timed out after {:.2} seconds.".format(time.time() - (startTime/1000)))
-        else:
-            print("Server responded after {} ms\n".format((endTime - startTime)))
-            havePingResponse = False #reset
+		if ( (not self.havePingResponse) ):
+		    view.display("No response from server, timed out after {:.2} seconds.".format(time.time() - (startTime/1000)))
+		else:
+		    view.display("Server responded after {} ms\n".format((self.endTime - startTime)))
+		    self.havePingResponse = False #reset
             
-    else: #if no special messages are found just default to sending chat type message
-        client.do_send({'speak':myname, 'txt':mytxt}) #default interaction, just name with text		
+	    else: #if no special messages are found just default to sending chat type message
+		self.do_send({'speak':myname, 'txt':mytxt}) #default interaction, just name with text	
+
 		
 		
-		
-		
-		
+	
+if __name__ == "__main__" :
+    
+    model = ClientModel()
+    view = ClientView()
+    control = ClientControl('localhost', 8888)
+    
+    control.start_control(model, view)
 		
 		
 		
